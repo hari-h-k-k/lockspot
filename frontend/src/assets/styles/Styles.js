09e1d32eedde648f1485ae899e3609decb0b5edc
Harikrishnan.K <harikdas2001@gmail.com>
2023-05-30 22:15:00 +0530

    packages install

---
+++ frontend/src/assets/styles/Styles.js
@@ -0,0 +1,78 @@
+// import {makeStyles} from "@mui/styles";
+//
+// export const justStyles = makeStyles((theme) => ({
+//     // Authentication
+//     selection: {
+//         borderWidth: 'medium',
+//         borderRadius: '99em',
+//         display: 'block',
+//         borderStyle: 'solid',
+//         borderColor: 'black',
+//         marginBottom: '12px',
+//         marginTop: '12px',
+//         width: '212px',
+//         padding: '6px 12px 8px 16px',
+//         backgroundColor: '#d9d9d9',
+//         strokeWidth: 2,
+//         color: 'black',
+//         opacity: 0.8,
+//         marginInline: 'auto'
+//     },
+//     dialogButton: {
+//         border: 'inherit',
+//         background: 'transparent',
+//         color: '#d96b05',
+//         fontWeight: 'bold',
+//         padding: 0,
+//         justifyContent: 'left'
+//     },
+//     innerBox: {
+//         borderBottom: 1,
+//         borderColor: 'black'
+//     },
+//     outerBox: {
+//         width: 'fit-content',
+//         marginInline: 'auto'
+//     },
+//     tab: {
+//         color: 'orange'
+//     },
+//     dialogContent: {
+//         display: 'flex',
+//         justifyContent: 'center',
+//         flexDirection: 'column',
+//         width: '400px',
+//         paddingTop: '0px',
+//         textAlign: 'center',
+//         alignItems: 'center',
+//         height: '600px',
+//         position: 'relative'
+//     },
+//     users: {
+//         border: "black",
+//         borderRightStyle: "solid",
+//         borderBottomStyle: "solid"
+//     },
+//     owners: {
+//         border: "black",
+//         borderBottomStyle: "solid"
+//     },
+//     // Search Bar
+//     paper: {
+//         p: '2px 4px',
+//         display: 'flex',
+//         alignItems: 'center',
+//         width: 400
+//     },
+//     iconButton: {
+//         p: '10px'
+//     },
+//     divider: {
+//         height: 28,
+//         m: 0.5
+//     },
+//     inputBase: {
+//         ml: 1,
+//         flex: 1
+//     },
+// }));
