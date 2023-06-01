1ccd7f732b03aa8257e928f3729b9ddaa3a9408c
Harikrishnan.K <harikdas2001@gmail.com>
2023-06-01 13:46:43 +0530

    Account button, logOutbutton UI fixed

--- frontend/src/chatGPT.js
+++ frontend/src/chatGPT.js
@@ -1,17 +1,28 @@
-import { Box, Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { Box,Text, Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 
 function MyMenu() {
     return (
+      <div style={{ display: "flex", alignItems: "center" }}>
+      <Text fontSize="xl" fontWeight="bold" mr={4}>
+        Welcome, email
+      </Text>
       <Menu>
-        <MenuButton as={Button} colorScheme="teal">
-          Open Menu
+        <MenuButton
+          as={Button}
+          colorScheme="red"
+          size="sm"
+          variant="outline"
+          _hover={{ bg: "red.500", color: "white" }}
+        >
+          Account
         </MenuButton>
-        <MenuList>
-          <MenuItem>Item 1</MenuItem>
-          <MenuItem>Item 2</MenuItem>
-          <MenuItem>Item 3</MenuItem>
+        <MenuList minW="150px" p={2} color="black">
+          <MenuItem _hover={{ bg: "teal.100" }}>Item 1</MenuItem>
+          <MenuItem _hover={{ bg: "teal.100" }}>Item 2</MenuItem>
+          <MenuItem _hover={{ bg: "teal.100" }}>Item 3</MenuItem>
         </MenuList>
       </Menu>
+    </div>
     );
   }
 
