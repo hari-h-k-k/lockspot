60a39796790121be26261ab09be82baed9d5cc7d
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-06-03 08:52:38 +0100

    Refactored files

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,23 +1,24 @@
 import './HomeStyle.css';
 import Navbar from '../navbar/Navbar.js';
 import BgImg from '../../assets/images/HomeBackground.jpg';
+
 function Home() {
-  return (
-    <div style={{
-      backgroundImage: `url(${BgImg})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      overflow: 'auto',
-      maxHeight: '100vh',
-      height: '100vh',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: '#fff',
-      fontSize: '2rem'
-    }}>
-    <Navbar className="navbar"/>
-    </div>
-  );
+    return (
+        <div style={{
+            backgroundImage: `url(${BgImg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            overflow: 'auto',
+            maxHeight: '100vh',
+            height: '100vh',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: '#fff',
+            fontSize: '2rem'
+        }}>
+            <Navbar className="navbar"/>
+        </div>
+    );
 }
 
 export default Home;
