df1d6305b14f9503b756c6777ae4847b51bb9ba6
SuryaSnG <surya.thottathil.2001@gmail.com>
2023-06-07 19:56:25 +0100

    Cache search results (Comments Removed)

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -4,35 +4,6 @@ import Navbar from '../navigation/Navbar.js';
 import Searchbar from '../search/Searchbar';
 import {useEffect, useState} from "react";
 import TurfSpace from "../turf/TurfSpace";
-//
-// function Home() {
-//     const [location, setLocation] = useState("");
-//
-//     useEffect(() => {
-//         if(location !== "") {
-//             sessionStorage.setItem('location', JSON.stringify(location))
-//         }
-//         if(JSON.parse(sessionStorage.getItem('location')) !== null) {
-//             setLocation(JSON.parse(sessionStorage.getItem('location')))
-//         }
-//     }, [location]);
-//
-//
-//     return (
-//         <div>
-//             <div className="homepage">
-//                 <Navbar className="navbar"/>
-//                 <Searchbar setLocation={setLocation}/>
-//             </div>
-//             {location &&
-//                 <TurfSpace turfLocation={location}/>
-//             }
-//         </div>
-//     );
-// }
-//
-// export default Home;
-
 
 function Home() {
     const [location, setLocation] = useState('');
